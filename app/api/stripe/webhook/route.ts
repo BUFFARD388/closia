ff10ab075902592aa2f3675aa511dfeb775f35c9
BUFFARD388 <laurent-buffard@wanadoo.fr>
2026-06-02 13:44:38 +0200

    fix: stripe types

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: NextRequest) {
   }
 
   if (event.type === 'checkout.session.completed') {
-    const session = event.data.object as Stripe.CheckoutSession
+    const session = event.data.object as any
     const { achatId, bienId, mode } = session.metadata!
 
     // Confirmer l'achat
